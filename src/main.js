96d0176491e03e0696696d7ac8f3f9c1523a5a51
Dynastiy <emmanuel00michael@gmail.com>
2022-04-19 11:17:39 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -3,10 +3,25 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+import '@/assets/css/main.css'
+import '@/assets/css/media-query.css'
+
+import "toastify-js/src/toastify.css"
+import Toastify from 'toastify-js'
+
+import instance from '@/plugins/axios.js'
+
+Vue.prototype.$axios = instance
+Vue.prototype.$toastify = Toastify
+    // Vue.use(http)
+
+Vue.config.ignoredElements = [/^ion-/]
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
---
+++ src/views/pages/completedTransactions.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+       <h3 class="text-bold">Completed Transactions</h3>
+             <div class="mt-4 other--tables">
+                     <div class="table-responsive">
+                            <table class="table table-centered table-nowrap mb-0">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Reference</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Amount (NGN)</th>
+                                    <th scope="col">Amount (BNB)</th>
+                                    <th scope="col">Payment Proof</th>
+                                    <th scope="col">Credit Proof</th>
+                                    <!-- <th scope="col">Status</th> -->
+                                    <!-- <th scope="col">Actions</th> -->
+                                </tr>
+                                </thead>
+                               <tbody>
+                                
+                                
+                                <tr v-if="loading">Fetching Data . . .</tr>
+                                <tr v-for="deposit in deposits.data" :key="deposit.id" v-else>
+                                     <td> {{ deposit.ref_no }} </td>
+                                    <td>{{ timeStamp(deposit.created_at) }} </td>
+                                    <td>&#8358;{{ nairaFilter(deposit.amount_naira) }} </td>
+                                    <td>{{ deposit.amount_bnb }}BNB</td>
+                                    <td> <a target="_blank" :href=" 'https://api.buybnb.io/' + deposit.payment_proof "> {{ deposit.payment_proof }} </a> </td>
+                                    <!-- <td> <span :class="[deposit.status]"> {{ deposit.status }} </span> </td> -->
+                                    <td v-if="deposit.payment_report"> <a target="_blank" :href=" 'https://api.buybnb.io/' + deposit.payment_report.credit_proof ">{{ deposit.payment_report.credit_proof }} </a> </td>
+                                    
+                                </tr>
+                                <tr v-show="deposits.total === 0 " class="text-danger">Nothing Here . . .</tr>
+                                </tbody>
+                            </table>
+                        </div>
+                </div>
+    </div>
+</template>
+
+
+<script>
+ import { nairaFilter, percentFilter, percentageFilter, timeStamp } from '@/plugins/filter.js'
+export default {
+    data(){
+        return {
+            nairaFilter, percentFilter, percentageFilter, timeStamp,
+            loading: false,
+            deposits: []
+        }
+    },
+    methods: {
+        async getDeposits(){
+            this.loading = true;
+            try {
+                let res = await this.$axios.get('/admin/get-deposits?status=completed')
+            console.log(res.data);
+            this.deposits = res.data
+            } catch (error) {
+                console.log(error);
+            }
+            this.loading = false
+        }
+    },
+    async created(){
+        this.getDeposits()
+    }
+}
+</script>
